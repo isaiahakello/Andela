2434cacaa7143930e447bcef69c9f226c3a54568
isaiahakello <isaiahakello19@gmail.com>
2017-01-17 15:31:47 +0300

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,29 +1,22 @@
- 7
-down vote
-	
+# Python program to check if the input number is prime or not
 
-def isprime(n):
-    '''check if integer n is a prime'''
+num = 407
 
-    # checks n is a positive integer
-    n = abs(int(n))
+# take input from the user
+# num = int(input("Enter a number: "))
 
-    # 0 and 1 are not primes
-    if n < 2:
-        return False
-
-    # 2 is the only even prime number
-    if n == 2: 
-        return True    
-
-    # all other even numbers are not primes
-    if not n & 1: 
-        return False
-
-    # range starts with 3 and only needs to go up 
-    # the square root of n for all odd numbers
-    for x in range(3, int(n**0.5) + 1, 2):
-        if n % x == 0:
-            return False
-
-    return True
+# prime numbers are greater than 1
+if num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+       
+# if input number is less than
+# or equal to 1, it is not prime
+else:
+   print(num,"is not a prime number")
